592c51f8579c9c83dee625dc1d162cc293d520b8
Mark Zwart <mark.zwart@pobox.com>
2018-04-20 00:36:46 +0200

    Removed repeating code by refactoring Added R Markdown report

--- forecast_functions.R
+++ forecast_functions.R
@@ -1,10 +1,16 @@
 # Function for displaying forecast and evaluating model ----
+#'
+#' @param forecast_model the fitted time series forecast model
+#' @param ts_full complete time series
+#' @return A list containing the plot with forecast points with full time series and a row with MASE
 evaluate_forecast <- function(forecast_model, ts_full){
   
+  # Make data frames from the time series objects
   df_forecast <- fortify(forecast_model) %>% 
     select(Index, Data = `Point Forecast`, `Lo 80`, `Hi 80`, `Lo 95`, `Hi 95`) 
   df_full <- fortify(ts_full) 
   
+  # Plot displaying the forecasted and full time series
   p_forecast <- ggplot(data = df_forecast, aes(x = Index)) + 
     geom_line(data = df_full, aes(y = Data), col = col_graydon[1]) + 
     geom_line(aes(y = Data), col = col_graydon[2]) + 
@@ -17,25 +23,37 @@ evaluate_forecast <- function(forecast_model, ts_full){
     labs(title = forecast_model$method, x = "", y = "", col = "") +
     theme_graydon("grid")
   
+  # Row of data for the method, and MASE of the test time series
   mase <- data.frame(method = forecast_model$method,
                      MASE = accuracy(forecast_model, ts_full)["Test set", "MASE"])
   
+  # Returning a list with the plot and MASE
   list(p_forecast = p_forecast,
        mase = mase)
 }
 
-# Comparing two time series ----
-plot_time_series <- function(ts_a, name_a, ts_b, name_b){
-  
-  df_full <- rbind(fortify(ts_a) %>% mutate(set = name_a),
-                   fortify(ts_b) %>% mutate(set = name_b))
+# Plot for comparing time series ----
+#'
+#' @param lst_timeseries list containing all time series
+#' @param vec_names vector containing the names that should be given to each time series
+#' @return A ggplot
+plot_time_series <- function(lst_timeseries, vec_names){
 
-  p_plot <- ggplot(df_full, aes(x = Index, y = Data)) + 
-    geom_line(aes(col = set)) + 
+  # Create a combined data frame of all time series objects
+  lst_df <- do.call("rbind", lapply(lst_timeseries, FUN = "fortify"))
+  # Create column naming each time series
+  vec_length <- sapply(lst_timeseries, "length")
+  tseries_names <- c(rep(vec_names, vec_length))
+  # Add column to the data frame
+  df_series <- cbind(tseries_names, lst_df)
+  
+  p_plot <- ggplot(df_series, aes(x = Index, y = Data)) + 
+    geom_line(aes(col = tseries_names)) + 
     scale_color_graydon() +
     scale_y_continuous(labels = format_number) +
-    labs(title = forecast_model$method, x = "", y = "", col = "") +
+    labs(x = "", y = "", col = "") +
     theme_graydon("grid")
   
-  return(p_plot)
+  return(p_plot)  
 }
+
